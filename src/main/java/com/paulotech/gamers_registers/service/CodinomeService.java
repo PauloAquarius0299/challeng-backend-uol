f66f14bbecc5c9bb5a6377ef72c3e873a01ef857
Paulo Cesar <paulocgsantos99@gmail.com>
2024-12-28 12:21:08 -0300

    feat: add Codinome Service

---
+++ src/main/java/com/paulotech/gamers_registers/service/CodinomeService.java
@@ -0,0 +1,39 @@
+package com.paulotech.gamers_registers.service;
+
+import com.paulotech.gamers_registers.domain.GrupoCodinome;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CodinomeService {
+    private final CodinomeRepositoryFactory codinomeRepositoryFactory;
+
+    public CodinomeService(CodinomeRepositoryFactory codinomeRepositoryFactory) {
+        this.codinomeRepositoryFactory = codinomeRepositoryFactory;
+    }
+
+    public String gerarCodinome(GrupoCodinome grupoCodinome, List<String> codinomesEmEnum) throws Exception {
+        var codinomesDisponiveis = listarCodinomesDisponiveis(grupoCodinome, codinomesEmEnum);
+        if(codinomesDisponiveis.isEmpty()){
+            throw new Exception("Não a codinomes disponiveis para esse grupo" + grupoCodinome.getNome());
+        }
+        var codinomeSorteado = sortearCodinome(codinomesDisponiveis);
+        return codinomeSorteado;
+    }
+
+    private List<String> listarCodinomesDisponiveis(GrupoCodinome grupoCodinome, List<String> codinomesEmEnum) {
+        var codinomes = buscarCodinomes(grupoCodinome);
+
+        var codinomesDisponiveis = codinomes
+                .stream()
+                .filter(codinome -> !codinomesEmEnum.contains(codinome))
+                .toList();
+        return codinomesDisponiveis;
+    }
+
+    private List<String> buscarCodinomes(GrupoCodinome grupoCodinome) {
+        var codinomeRepository = codinomeRepositoryFactory.create(grupoCodinome);
+        return List.of();
+    }
+}
