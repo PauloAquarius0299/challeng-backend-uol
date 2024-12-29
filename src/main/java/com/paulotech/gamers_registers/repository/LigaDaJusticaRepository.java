d4784b4c030ae5f611774c26c9152e162e9ba161
Paulo Cesar <paulocgsantos99@gmail.com>
2024-12-29 12:01:31 -0300

    fix: adjust codinomes

--- src/main/java/com/paulotech/gamers_registers/repository/LigaDaJusticaRepository.java
+++ src/main/java/com/paulotech/gamers_registers/repository/LigaDaJusticaRepository.java
@@ -3,16 +3,15 @@ package com.paulotech.gamers_registers.repository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.paulotech.gamers_registers.domain.GrupoCodinome;
+import com.paulotech.gamers_registers.repository.dto.CodinomeDTO;
 import com.paulotech.gamers_registers.repository.dto.LigaDaJusticaDTO;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
-
 @Repository
 public class LigaDaJusticaRepository implements CodinomeRepository{
     @Override
-    public List<String> buscarCodinomes() throws JsonProcessingException {
+    public CodinomeDTO buscarCodinomes() throws JsonProcessingException {
         var codinomes = RestClient.builder()
                 .baseUrl(GrupoCodinome.LIGA_DA_JUSTICA.getUri())
                 .build()
@@ -21,6 +20,6 @@ public class LigaDaJusticaRepository implements CodinomeRepository{
                 .body(String.class);
         var xmlMapper = new XmlMapper();
         var ligaDaJustica = xmlMapper.readValue(codinomes, LigaDaJusticaDTO.class);
-        return ligaDaJustica.getCodinomes();
+        return ligaDaJustica;
     }
 }
